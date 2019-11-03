d3346815bf537213869651b2758b1be1bed47035
Ana Córcoles <ana.corcoles@gmail.com>
2019-11-03 19:11:11 +0100

    Configuramos Cors en Spring Security OAuth2

--- src/main/java/org/sanchez/corcoles/ana/pruebasconcepto/zuul/oauth/configuration/ResourceServerConfig.java
+++ src/main/java/org/sanchez/corcoles/ana/pruebasconcepto/zuul/oauth/configuration/ResourceServerConfig.java
@@ -1,8 +1,10 @@
 package org.sanchez.corcoles.ana.pruebasconcepto.zuul.oauth.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,6 +12,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -36,8 +44,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers("/api/productos/productos/**",
                         "/api/usuarios/usuarios/**",
                         "/api/items/items/**").hasRole("ADMIN")
-
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().cors().configurationSource(corsConfigurationSource());
                 /*.antMatchers(HttpMethod.POST, "/api/productos/productos",
                         "/api/usuarios/usuarios",
                         "/api/items/items").hasRole("ADMIN")
@@ -47,6 +55,29 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE, "/api/productos/productos/{id}",
                         "/api/usuarios/usuarios/{id}",
                         "/api/items/items/{id}").hasRole("ADMIN");*/
+    }
+
+    //Configuración del cors para todas las rutas (/**)
+    @Bean("corsConfigurationSource")
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); //Se pueden añadir los nombres de dominios, también se puede añadir con corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList(HttpMethod.POST.name(), HttpMethod.GET.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name()));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return urlBasedCorsConfigurationSource;
+    }
+
+    //Configuramos un filtro para que se aplique de forma global
+    @Bean("corsFiltern")
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        final FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        corsFilterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return corsFilterRegistrationBean;
     }
 
     @Bean("jwtTokenStore")
